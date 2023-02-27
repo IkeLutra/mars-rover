5cfc59a62553a17e9611698dcd55af6ad5b80b4a
Will Eldrid-Otterburn <will.eldrid-otterburn@deliveroo.co.uk>
2023-02-27 21:01:34 +0000

    processing commands

--- src/parser.rs
+++ src/parser.rs
@@ -29,7 +29,7 @@ enum Command {
     Right,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     North,
     East,
@@ -77,6 +77,43 @@ impl Robot {
             commands: commands,
         }
     }
+
+    pub fn process(self, grid: Grid) -> (i32, i32, Direction, bool) {
+        let mut current_x = self.initial_x;
+        let mut current_y = self.initial_y;
+        let mut current_direction = self.initial_orientation;
+        let mut is_lost = false;
+        for command in self.commands {
+            let (x, y, direction) = match command {
+                Command::Forward => match current_direction {
+                    Direction::North => (current_x, current_y + 1, current_direction),
+                    Direction::East => (current_x + 1, current_y, current_direction),
+                    Direction::South => (current_x, current_y - 1, current_direction),
+                    Direction::West => (current_x - 1, current_y, current_direction),
+                },
+                Command::Left => match current_direction {
+                    Direction::North => (current_x, current_y, Direction::West),
+                    Direction::East => (current_x, current_y, Direction::North),
+                    Direction::South => (current_x, current_y, Direction::East),
+                    Direction::West => (current_x, current_y, Direction::South),
+                },
+                Command::Right => match current_direction {
+                    Direction::North => (current_x, current_y, Direction::East),
+                    Direction::East => (current_x, current_y, Direction::South),
+                    Direction::South => (current_x, current_y, Direction::West),
+                    Direction::West => (current_x, current_y, Direction::North),
+                },
+            };
+            if x < 0 || x > grid.max_x || y < 0 || y > grid.max_y {
+                is_lost = true;
+                break;
+            }
+            current_x = x;
+            current_y = y;
+            current_direction = direction;
+        }
+        (current_x, current_y, current_direction, is_lost)
+    }
 }
 
 #[cfg(test)]
@@ -105,5 +142,51 @@ mod test {
         };
         let actual = Robot::parse("(2, 3, E) LFRFF".to_string());
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_process_1() {
+        let robot = Robot {
+            initial_x: 2,
+            initial_y: 3,
+            initial_orientation: Direction::East,
+            commands: vec![
+                Command::Left,
+                Command::Forward,
+                Command::Right,
+                Command::Forward,
+                Command::Forward,
+            ],
+        };
+        let grid = Grid { max_x: 4, max_y: 8 };
+        let (x, y, direction, is_lost) = robot.process(grid);
+        assert_eq!(x, 4);
+        assert_eq!(y, 4);
+        assert_eq!(direction, Direction::East);
+        assert_eq!(is_lost, false);
+    }
+
+    #[test]
+    fn test_process_2() {
+        let robot = Robot {
+            initial_x: 0,
+            initial_y: 2,
+            initial_orientation: Direction::North,
+            commands: vec![
+                Command::Forward,
+                Command::Forward,
+                Command::Left,
+                Command::Forward,
+                Command::Right,
+                Command::Forward,
+                Command::Forward,
+            ],
+        };
+        let grid = Grid { max_x: 4, max_y: 8 };
+        let (x, y, direction, is_lost) = robot.process(grid);
+        assert_eq!(x, 0);
+        assert_eq!(y, 4);
+        assert_eq!(direction, Direction::West);
+        assert_eq!(is_lost, true);
     }
 }
